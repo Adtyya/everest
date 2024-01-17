f7b3cd3b8e472b87955c3484db388b5bdd7dd5da
aditya <aditdualapan@gmail.com>
2024-01-17 15:13:05 +0700

    add simple animation

--- components/box/reservation/cardProduct.jsx
+++ components/box/reservation/cardProduct.jsx
@@ -20,9 +20,9 @@ export default function CardProduct({ image, title, subtitle, rtl }) {
           fetchPriority="low"
           className="z-10"
         />
-        <div className="absolute top-0 left-10 h-14 w-14 rounded-full bg-dot-gradient"></div>
-        <div className="absolute bottom-0 right-10 h-10 w-10 rounded-full bg-dot-gradient"></div>
-        <div className="w-64 h-64 bg-gradient-card rounded-full"></div>
+        <div className="absolute top-0 left-10 group-hover:h-10 group-hover:w-10 duration-300 h-14 w-14 rounded-full bg-dot-gradient"></div>
+        <div className="absolute bottom-0 right-10 h-10 w-10 group-hover:w-14 group-hover:h-14 duration-300 rounded-full bg-dot-gradient z-10"></div>
+        <div className="w-72 h-72 bg-gradient-card rounded-full group-hover:w-80 group-hover:h-80 group-hover:skew-x-6 duration-300"></div>
       </div>
       <div className="space-y-5">
         <HeadingThree className="text-typ-gray">{title}</HeadingThree>
