b531d910be372b7f01c298bfc8086848259b3d65
AhmadHasbyBik <abik262002@gmail.com>
2024-01-25 14:26:01 +0700

    add delivery page

---
+++ app/delivery/page.js
@@ -0,0 +1,14 @@
+import Hero from "@/components/view/delivery/hero";
+import ProductList from "@/components/view/delivery/productList";
+
+export default function DeliveryPage() {
+  return (
+    <main>
+      <Hero></Hero>
+      <br></br>
+      <ProductList></ProductList>
+      <br></br>
+      <br></br>
+    </main>
+  );
+}
---
+++ components/view/delivery/content/productList.js
@@ -0,0 +1,25 @@
+export const listProduct = [
+  {
+    title:
+      "Kemudahan Pemesanan",
+    subtitle:
+      "Sekarang Anda dapat melakukan pemesanan produk favorit Anda dengan cepat dan mudah melalui aplikasi seluler atau situs web kami, tanpa harus meninggalkan rumah Anda.",
+    image: "/images/reservation/product-1.png",
+    rtl: false,
+  },
+  {
+    title: "Metode Pembayaran Fleksibel",
+    subtitle:
+      "Kami berkomitmen untuk mengantarkan pesanan Anda dengan cepat dan aman, sehingga Anda dapat menikmati produk yang Anda cintai dalam waktu singkat.",
+    image: "/images/reservation/product-2.png",
+    rtl: true,
+  },
+  {
+    title:
+      "Tingkatkan Efisiensi Operasional dengan Integrasi POS yang mulus dari Everest",
+    subtitle:
+      "Bayar dengan mudah melalui berbagai metode pembayaran, termasuk kartu kredit, transfer bank, dan opsi pembayaran digital.",
+    image: "/images/reservation/product-3.png",
+    rtl: false,
+  },
+];
